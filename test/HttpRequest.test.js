30a9113bd99e5ccbd0093736d40e5fe5554f3179
Alex Guyot <ajguyot@icloud.com>
2018-10-05 21:58:57 -0500

    Fully rebuilt HttpRequest, Jest test suite, CircleCI config, npm version 0.0.0

---
+++ test/HttpRequest.test.js
@@ -0,0 +1,242 @@
+'use strict';
+
+const _ = require('@unplgtc/standard-promise');
+const HttpRequest = require('./../src/HttpRequest');
+const rp = require('request-promise-native');
+const StandardError = require('@unplgtc/standard-error');
+
+jest.mock('request-promise-native');
+
+var simpleGetDeletePayload = {
+	url: 'test_url',
+	headers: {
+		header1: 'test_header'
+	},
+	json: true
+}
+
+var simplePutPostPayload = {
+	url: 'test_url',
+	headers: {
+		header1: 'test_header'
+	},
+	body: {
+		testing: true
+	},
+	json: true
+}
+
+var payloadWithoutUrl = {
+	headers: {
+		header1: 'test_header'
+	},
+	json: true
+}
+
+var simpleMockedResponse = {
+	testing: true
+}
+
+test('Can access the payload', async() => {
+	// Setup
+	rp.get.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.build(simpleGetDeletePayload);
+
+	// Test
+	expect(req.payload).toEqual(simpleGetDeletePayload);
+});
+
+test('Can send a GET request', async() => {
+	// Setup
+	rp.get.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.build(simpleGetDeletePayload);
+
+	// Execute
+	var res = await req.get();
+	var res2 = await _(req.get());
+
+	// Test
+	expect(rp.get).toHaveBeenCalledWith(req.payload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+	expect(res2.err).toBe(undefined);
+	expect(res2.data).toBe(simpleMockedResponse);
+});
+
+test('Can send a POST request', async() => {
+	// Setup
+	rp.post.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.build(simplePutPostPayload);
+
+	// Execute
+	var res = await req.post();
+	var res2 = await _(req.post());
+
+	// Test
+	expect(rp.post).toHaveBeenCalledWith(req.payload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+	expect(res2.err).toBe(undefined);
+	expect(res2.data).toBe(simpleMockedResponse);
+});
+
+test('Can send a PUT request', async() => {
+	// Setup
+	rp.put.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.build(simplePutPostPayload);
+
+	// Execute
+	var res = await req.put();
+	var res2 = await _(req.put());
+
+	// Test
+	expect(rp.put).toHaveBeenCalledWith(req.payload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+	expect(res2.err).toBe(undefined);
+	expect(res2.data).toBe(simpleMockedResponse);
+});
+
+test('Can send a DELETE request', async() => {
+	// Setup
+	rp.delete.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.build(simpleGetDeletePayload);
+
+	// Execute
+	var res = await req.delete();
+	var res2 = await _(req.delete());
+
+	// Test
+	expect(rp.delete).toHaveBeenCalledWith(req.payload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+	expect(res2.err).toBe(undefined);
+	expect(res2.data).toBe(simpleMockedResponse);
+});
+
+test('Can build and send a request in one line', async() => {
+	// Setup
+	rp.get.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+
+	// Execute
+	var res = await req.build(simpleGetDeletePayload).get();
+
+	// Test
+	expect(rp.get).toHaveBeenCalledWith(req.payload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+});
+
+test('StandardError returned when request is made with empty payload', async() => {
+	// Setup
+	var req = Object.create(HttpRequest);
+	req.build();
+
+	// Execute
+	var res = await _(req.get());
+
+	// Test
+	expect(rp.get).not.toHaveBeenCalled();
+	expect(res.err).toEqual(StandardError.HttpRequestExecutor_400);
+	expect(res.data).toBe(undefined);
+});
+
+test('StandardError returned when request is made with empty url', async() => {
+	// Setup
+	var req = Object.create(HttpRequest);
+	req.build(payloadWithoutUrl);
+
+	// Execute
+	var res = await _(req.get());
+
+	// Test
+	expect(rp.get).not.toHaveBeenCalled();
+	expect(res.err).toEqual(StandardError.HttpRequestExecutor_400);
+	expect(res.data).toBe(undefined);
+});
+
+test('Can execute a request directly without automatic StandardPromise', async() => {
+	// Setup
+	rp.get.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.build(simpleGetDeletePayload);
+
+	// Execute
+	var res = await req.execute('get');
+	var res2 = await _(req.execute('get'));
+
+	// Test
+	expect(rp.get).toHaveBeenCalledWith(req.payload);
+	expect(res).toBe(simpleMockedResponse);
+	expect(res2.err).toBe(undefined);
+	expect(res2.data).toBe(simpleMockedResponse);
+});
+
+test('Can assemble a request piece by piece', async() => {
+	// Setup
+	rp.post.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.setUrl(simplePutPostPayload.url);
+	req.setHeaders(simplePutPostPayload.headers);
+	req.setBody(simplePutPostPayload.body);
+	req.setJson(simplePutPostPayload.json);
+
+	// Execute
+	var res = await req.post();
+
+	// Test
+	expect(rp.post).toHaveBeenCalledWith(simplePutPostPayload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+});
+
+test('Can chain the piece by piece assembly of a request', async() => {
+	// Setup
+	rp.post.mockResolvedValue(simpleMockedResponse);
+
+	var req = Object.create(HttpRequest);
+	req.setUrl(simplePutPostPayload.url)
+	   .setHeader('header1', simplePutPostPayload.headers.header1)
+	   .setBody(simplePutPostPayload.body)
+	   .setJson(simplePutPostPayload.json);
+
+	// Execute
+	var res = await req.post();
+
+	// Test
+	expect(rp.post).toHaveBeenCalledWith(simplePutPostPayload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+});
+
+test('Can chain creation, assembly, and execution of an HttpRequest', async() => {
+	// Setup
+	rp.post.mockResolvedValue(simpleMockedResponse);
+
+	// Execute
+	var res = await HttpRequest.create()
+	                           .setUrl(simplePutPostPayload.url)
+	                           .setHeaders(simplePutPostPayload.headers)
+	                           .setBody(simplePutPostPayload.body)
+	                           .setJson(simplePutPostPayload.json)
+	                           .post();
+
+	// Test
+	expect(rp.post).toHaveBeenCalledWith(simplePutPostPayload);
+	expect(res.err).toBe(undefined);
+	expect(res.data).toBe(simpleMockedResponse);
+});
